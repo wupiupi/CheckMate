79d5fce43cd72747717796111656d6d06afaf4f5
Paul <pasha.makey@gmail.com>
2024-02-18 21:17:29 +0300

    ✨ Project: Changes

---
+++ CheckMate/Models/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  CheckMate
+//
+//  Created by Paul Makey on 18.02.24.
+//
+
+import Foundation
---
+++ CheckMate/Services/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  CheckMate
+//
+//  Created by Paul Makey on 18.02.24.
+//
+
+import Foundation

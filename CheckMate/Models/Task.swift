36c33ceffc6c51f4c9d8a848065fd467a9961b0f
Paul <pasha.makey@gmail.com>
2024-02-18 21:18:21 +0300

    🔧 chore: Added Task & TaskList models

--- CheckMate/Models/Task.swift
+++ CheckMate/Models/Task.swift
@@ -5,4 +5,17 @@
 //  Created by Paul Makey on 18.02.24.
 //
 
+import RealmSwift
 import Foundation
+
+final class TaskList: Object {
+    @Persisted var title = ""
+    @Persisted var data = Date()
+    @Persisted var tasks: List<Task>
+}
+
+final class Task: Object {
+    @Persisted var title = ""
+    @Persisted var note = ""
+    @Persisted var data = Date()
+}

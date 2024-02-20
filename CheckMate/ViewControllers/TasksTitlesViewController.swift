8bf46fdd841f4bd5dbbd909f529b127ce5ceac70
Paul <pasha.makey@gmail.com>
2024-02-21 02:13:31 +0300

    🔧 chore: Method to build alert controller / add button for navigationBar

--- CheckMate/ViewControllers/TasksTitlesViewController.swift
+++ CheckMate/ViewControllers/TasksTitlesViewController.swift
@@ -9,17 +9,47 @@ import UIKit
 import RealmSwift
 
 final class TasksTitlesViewController: UITableViewController {
-
+    
+    // MARK: - Private properties
     private let storageManager = StorageManager.shared
     private var taskTitles: Results<TaskTitle>!
     
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         taskTitles = storageManager.fetchTasksTitles(TaskTitle.self)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addButtonDidTapped)
+        )
     }
-
+    
+    @objc func addButtonDidTapped() {
+        showAlertController()
+    }
 }
 
+// MARK: - Private Methods
+private extension TasksTitlesViewController {
+    func showAlertController() {
+        let alertBuilder = AlertControllerBuilder(
+            title: "New task",
+            message: "What do you wanna do?"
+        )
+        
+        let alertController = alertBuilder
+            .addTextField(text: "", placeholder: "Type something...")
+            .addAction(title: "Save", style: .default)
+            .addAction(title: "Cancel", style: .destructive)
+            .build()
+        
+        present(alertController, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
 extension TasksTitlesViewController {
     override func tableView(
         _ tableView: UITableView,

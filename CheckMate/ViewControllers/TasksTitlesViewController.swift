6c859d57f784fc0b904c48d60b39b5e40b05a173
Paul <pasha.makey@gmail.com>
2024-02-20 20:10:59 +0300

    🔧 chore: Work with UITableViewDataSource methods

--- CheckMate/ViewControllers/TasksTitlesViewController.swift
+++ CheckMate/ViewControllers/TasksTitlesViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import RealmSwift
 
-class TasksTitlesViewController: UIViewController {
+final class TasksTitlesViewController: UITableViewController {
 
+    private let storageManager = StorageManager.shared
+    private var taskTitles: Results<TaskTitle>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        taskTitles = storageManager.fetchTasksTitles(TaskTitle.self)
     }
-
 
 }
 
+extension TasksTitlesViewController {
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        taskTitles.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "TaskTitleCell",
+            for: indexPath
+        )
+        
+        let taskTitle = taskTitles[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = taskTitle.title
+        content.secondaryText = taskTitle.tasks.count.formatted()
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+}
